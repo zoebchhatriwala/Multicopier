1d9eb5d426042547c2210af66304679f33a0bf27
unknown <Zoeb Chhatriwala>
2014-08-06 21:02:28 +0530

    first commit

---
+++ Multicopier/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Multicopier
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.items = new System.Windows.Forms.ListBox();
+            this.copy = new System.Windows.Forms.Button();
+            this.del = new System.Windows.Forms.Button();
+            this.hide = new System.Windows.Forms.Button();
+            this.clr = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.about = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tab1 = new System.Windows.Forms.TabPage();
+            this.tab2 = new System.Windows.Forms.TabPage();
+            this.items2 = new System.Windows.Forms.ListBox();
+            this.tabControl1.SuspendLayout();
+            this.tab1.SuspendLayout();
+            this.tab2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // items
+            // 
+            this.items.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.items.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.items.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.items.ForeColor = System.Drawing.SystemColors.Info;
+            this.items.FormattingEnabled = true;
+            this.items.HorizontalScrollbar = true;
+            this.items.ItemHeight = 19;
+            this.items.Location = new System.Drawing.Point(0, 1);
+            this.items.Name = "items";
+            this.items.Size = new System.Drawing.Size(561, 384);
+            this.items.TabIndex = 0;
+            // 
+            // copy
+            // 
+            this.copy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.copy.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.copy.Location = new System.Drawing.Point(596, 129);
+            this.copy.Name = "copy";
+            this.copy.Size = new System.Drawing.Size(104, 36);
+            this.copy.TabIndex = 1;
+            this.copy.Text = "Copy";
+            this.copy.UseVisualStyleBackColor = true;
+            this.copy.Click += new System.EventHandler(this.copy_Click);
+            // 
+            // del
+            // 
+            this.del.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.del.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.del.Location = new System.Drawing.Point(597, 171);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(104, 36);
+            this.del.TabIndex = 2;
+            this.del.Text = "Delete Item";
+            this.del.UseVisualStyleBackColor = true;
+            this.del.Click += new System.EventHandler(this.del_Click);
+            // 
+            // hide
+            // 
+            this.hide.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.hide.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.hide.Location = new System.Drawing.Point(596, 255);
+            this.hide.Name = "hide";
+            this.hide.Size = new System.Drawing.Size(104, 36);
+            this.hide.TabIndex = 3;
+            this.hide.Text = "Hide";
+            this.hide.UseVisualStyleBackColor = true;
+            this.hide.Click += new System.EventHandler(this.hide_Click);
+            // 
+            // clr
+            // 
+            this.clr.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.clr.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.clr.Location = new System.Drawing.Point(596, 213);
+            this.clr.Name = "clr";
+            this.clr.Size = new System.Drawing.Size(104, 36);
+            this.clr.TabIndex = 4;
+            this.clr.Text = "Clear All";
+            this.clr.UseVisualStyleBackColor = true;
+            this.clr.Click += new System.EventHandler(this.clr_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // start
+            // 
+            this.start.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.start.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.start.Location = new System.Drawing.Point(597, 45);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(104, 36);
+            this.start.TabIndex = 5;
+            this.start.Text = "Start Tracking";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.stop.Enabled = false;
+            this.stop.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.stop.Location = new System.Drawing.Point(596, 87);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(104, 36);
+            this.stop.TabIndex = 6;
+            this.stop.Text = "Stop Tracking";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // about
+            // 
+            this.about.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.about.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.about.Location = new System.Drawing.Point(596, 297);
+            this.about.Name = "about";
+            this.about.Size = new System.Drawing.Size(104, 36);
+            this.about.TabIndex = 7;
+            this.about.Text = "About";
+            this.about.UseVisualStyleBackColor = true;
+            this.about.Click += new System.EventHandler(this.about_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Running Here";
+            this.notifyIcon1.BalloonTipTitle = "Multicopier";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Multicopier";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tab1);
+            this.tabControl1.Controls.Add(this.tab2);
+            this.tabControl1.Location = new System.Drawing.Point(13, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(569, 411);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tab1
+            // 
+            this.tab1.Controls.Add(this.items);
+            this.tab1.Location = new System.Drawing.Point(4, 22);
+            this.tab1.Name = "tab1";
+            this.tab1.Padding = new System.Windows.Forms.Padding(3);
+            this.tab1.Size = new System.Drawing.Size(561, 385);
+            this.tab1.TabIndex = 0;
+            this.tab1.Text = "Text";
+            this.tab1.UseVisualStyleBackColor = true;
+            // 
+            // tab2
+            // 
+            this.tab2.Controls.Add(this.items2);
+            this.tab2.Location = new System.Drawing.Point(4, 22);
+            this.tab2.Name = "tab2";
+            this.tab2.Padding = new System.Windows.Forms.Padding(3);
+            this.tab2.Size = new System.Drawing.Size(561, 385);
+            this.tab2.TabIndex = 1;
+            this.tab2.Text = "Files";
+            this.tab2.UseVisualStyleBackColor = true;
+            // 
+            // items2
+            // 
+            this.items2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.items2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.items2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.items2.ForeColor = System.Drawing.SystemColors.Info;
+            this.items2.FormattingEnabled = true;
+            this.items2.HorizontalScrollbar = true;
+            this.items2.ItemHeight = 19;
+            this.items2.Location = new System.Drawing.Point(0, 1);
+            this.items2.Name = "items2";
+            this.items2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.items2.Size = new System.Drawing.Size(561, 384);
+            this.items2.TabIndex = 2;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(713, 435);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.about);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.clr);
+            this.Controls.Add(this.hide);
+            this.Controls.Add(this.del);
+            this.Controls.Add(this.copy);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(729, 474);
+            this.Name = "Home";
+            this.Text = "Multicopier";
+            this.tabControl1.ResumeLayout(false);
+            this.tab1.ResumeLayout(false);
+            this.tab2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox items;
+        private System.Windows.Forms.Button copy;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.Button hide;
+        private System.Windows.Forms.Button clr;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button about;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tab1;
+        private System.Windows.Forms.TabPage tab2;
+        private System.Windows.Forms.ListBox items2;
+    }
+}
+
---
+++ Multicopier/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multicopier
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+            CenterToScreen();
+        }
+        
+        
+
+        private void copy_Click(object sender, EventArgs e)
+        {
+             try
+             {
+                 if (tabControl1.SelectedIndex == 0)
+                 {
+                     Clipboard.SetText(items.SelectedItem.ToString());
+                 }
+                 else
+                 {
+                     StringCollection file = new StringCollection();
+                     foreach (string x in items2.SelectedItems)
+                     {
+                         file.Add(x);
+                     }
+                    Clipboard.SetFileDropList(file);
+                 }
+             }
+             catch (Exception) { MessageBox.Show("No Item Selected","Error");}
+        }
+
+        private void del_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 0)
+            {
+                items.Items.Remove(items.SelectedItem);
+            }
+            else if (tabControl1.SelectedIndex == 1)
+            {
+                items2.Items.Remove(items2.SelectedItem);
+            }
+        }
+
+        private void clr_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 0)
+            {
+                items.Items.Clear();
+            }
+            else if (tabControl1.SelectedIndex == 1)
+            {
+                items2.Items.Clear();
+            }
+        }
+
+        private void hide_Click(object sender, EventArgs e)
+        {
+            this.Opacity = 0;
+            this.ShowInTaskbar = false;
+            notifyIcon1.ShowBalloonTip(2000);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           if (items.Items.Contains(Clipboard.GetText())==false && Clipboard.GetText()!="")
+            {
+              items.Items.Insert(0, Clipboard.GetText());
+            }
+           else if (Clipboard.ContainsFileDropList()==true)
+           {
+            StringCollection file = new StringCollection();
+            file = Clipboard.GetFileDropList();
+            foreach (string x in file)
+                {
+                    if (items2.Items.Contains(x) == false)
+                    { items2.Items.Insert(0, x); }
+                }
+            }
+            
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            start.Enabled = false;
+            Home.ActiveForm.Text = "Multicopier [Running]";
+            stop.Enabled = true;
+            timer1.Enabled = true;
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            start.Enabled = true;
+            Home.ActiveForm.Text = "Multicopier";
+            stop.Enabled = false;
+            timer1.Enabled = false;
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Opacity = 1;
+            this.ShowInTaskbar = true;
+        }
+
+        private void about_Click(object sender, EventArgs e)
+        {
+          MessageBox.Show("Multicopier is a Application That can be Used for Copying Multiple Data From Multiple Sources,\nMulticopier Tracks Your Copy Record for Future Refrence Until Application Closes.\nApplication Developed By: Zoeb Chhatriwala");
+        }
+    }
+}
